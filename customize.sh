638440a3a04fa54cea6093ded27f779cd8000ea3
r4nd0mwOrK933 <83849873+r4nd0mwOrK933@users.noreply.github.com>
2021-08-15 19:38:02 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,11 +6,17 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.99.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Custom settings
 #cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
 #cp -f package/litte/banner package/base-files/files/etc/banner
 #cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
 #./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+
+# add compilation time to bin filename
+sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +'%F')/g' include/image.mk
+# 修改机器名称
+sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
 
